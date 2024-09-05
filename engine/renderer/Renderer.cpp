976f1adecf9b8ff840718482512451dfe8b38200
Ben Lewin <benlewin7@gmail.com>
2024-09-06 00:38:04 +0100

    Implemented Blending Abstraction for RGBA and RGB channels

--- engine/renderer/Renderer.cpp
+++ engine/renderer/Renderer.cpp
@@ -6,6 +6,10 @@ namespace Ugly {
 
     Renderer::SceneData* Renderer::s_SceneData = new Renderer::SceneData;
 
+    void Renderer::Init(){
+		RenderCommand::Init();
+    }
+
     void Renderer::BeginScene(OrthographicCamera& camera)
     {
         s_SceneData->ViewProjectionMatrix = camera.GetViewProjectionMatrix();
--- engine/renderer/Renderer.h
+++ engine/renderer/Renderer.h
@@ -8,6 +8,7 @@ namespace Ugly {
     class Renderer 
     {
         public:
+            static void Init();
             static void BeginScene(OrthographicCamera& camera);
             static void EndScene();
 
