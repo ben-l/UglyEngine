1daf74e3953bc7877a2f65f2477dbce16eb8c3c7
Ben Lewin <benlewin7@gmail.com>
2024-09-09 05:36:53 +0100

    Created a single shader to replace the 2 separate shaders

--- engine/renderer/Texture.cpp
+++ engine/renderer/Texture.cpp
@@ -4,12 +4,24 @@
 #include "OpenGLTexture.h"
 
 namespace Ugly {
+
+    Ref<Texture2d> Texture2d::Create(uint32_t width, uint32_t height)
+    {
+        switch (Renderer::GetAPI())
+        {
+            case RendererAPI::API::None: UE_CORE_ASSERT(false, "RendererAPI::None is currently not supported"); return nullptr;
+            case RendererAPI::API::OpenGL: return CreateRef<OpenGLTexture2D>(width, height);
+        }
+        UE_CORE_ASSERT(false, "Unknown RendererAPI!");
+        return nullptr;
+    }
+
     Ref<Texture2d> Texture2d::Create(const std::string& path){
 
         switch (Renderer::GetAPI())
         {
             case RendererAPI::API::None: UE_CORE_ASSERT(false, "RendererAPI::None is currently not supported"); return nullptr;
-            case RendererAPI::API::OpenGL: return std::make_shared<OpenGLTexture2D>(path);
+            case RendererAPI::API::OpenGL: return CreateRef<OpenGLTexture2D>(path);
         }
         UE_CORE_ASSERT(false, "Unknown RendererAPI!");
         return nullptr;
